41cfa62c024992ce75dda883d38c6ad622523aaa
Mingges-Khilfatul <minggeskhilfatul2811@gmail.com>
2023-04-17 17:23:10 +0700

    Commit Edited

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 print('Tugas Belajar Terstruktur 01')
 nama = input('Nama Lengkap :')
 Kelas = input('Asal Kelas :')
-
-print('Halo, saya '+ nama + ' Saya berasal dari kelas ' + Kelas)
+NIM = input('NIM :')
+print('Halo, saya '+ nama + ' Saya berasal dari kelas ' + Kelas + 'dengan NIM sebagai berikut' + NIM)
 
 value = 34000
 value2 = 96000
 print(value+value2)
-
+print(value*value2)
