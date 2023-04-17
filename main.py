cb96a7582d3e2402b1121a7e37c8e765078639ea
Mingges-Khilfatul <minggeskhilfatul2811@gmail.com>
2023-04-17 17:21:50 +0700

    First Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print('Tugas Belajar Terstruktur 01')
+nama = input('Nama Lengkap :')
+Kelas = input('Asal Kelas :')
+
+print('Halo, saya '+ nama + ' Saya berasal dari kelas ' + Kelas)
+
+value = 34000
+value2 = 96000
+print(value+value2)
+
